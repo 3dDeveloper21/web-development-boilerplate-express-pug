c03811b519f119a1dc0759df1fb4a3ef6a3c9b78
Harpreet Singh <3ddeveloper21@gmail.com>
2023-02-03 16:00:25 -0500

    app.js not connecting to Prismic

--- app.js
+++ app.js
@@ -1,150 +1,202 @@
-const path = require('path')
-const express = require('express')
-const app = express()
-const port = 3000
-
-// node-fetch is used to make network requests to the Prismic Rest API. 
-// In Node.js Prismic projects, you must provide a fetch method to the
-// Prismic client.
-const fetch = require('node-fetch')
-const Prismic = require('@prismicio/client')
-const PrismicH = require('@prismicio/helpers')
-
 require('dotenv').config();
 
-// Prismic connection information
-const repoName = process.env.PRISMIC_ENDPOINT;
-const accessToken = process.env.PRISMIC_ACCESS_TOKEN;
+const fetch = require('node-fetch');
+const logger = require('morgan');
+const path = require('path');
+const express = require('express');
+const errorHandler = require('errorhandler');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
-// The `routes` property is your Route Resolver. It defines how you will 
-// structure URLs in your project. Update the types to match the Custom 
-// Types in your project, and edit the paths to match the routing in your 
-// project.
-const routes = [
-  {
-    type: 'home',
-    path: '/',
-  },
-]
+const app = express();
+const port = process.env.PORT || 3000;
 
-const client = Prismic.createClient(repoName, { 
-  fetch, 
-  accessToken,
-  routes,
-})
+const Prismic = require('@prismicio/client');
+const PrismicH = require('@prismicio/helpers');
+const { application } = require('express');
+const UAParser = require('ua-parser-js');
 
-// Set pug as templating engine
-app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, 'views'))
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(errorHandler());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Add a middleware function that runs on every route. It will inject 
-// the prismic context to the locals so that we can access these in 
-// our templates.
-app.use((req, res, next) => {
-  res.locals.ctx = {
-    PrismicH,
+// Initialize the prismic.io api
+const initApi = (req) => {
+  return Prismic.createClient(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req,
+    fetch,
+  });
+};
+
+// Link Resolver
+const HandleLinkResolver = (doc) => {
+  if (doc.type === 'product') {
+    return `/detail/${doc.slug}`;
   }
-  next()
-})
 
-// Query for the root path.
-app.get('/', async (req, res) => {
-  // Here we are retrieving the first document from your API endpoint
-  await client.getSingle('about').then(response => {
-    const { data } = response;
-    console.log(data)
-    res.render('pages/home', { document: data })
-  })
-  
+  if (doc.type === 'collections') {
+    return '/collections';
+  }
 
-  // console.log(document.data)
-})
+  if (doc.type === 'about') {
+    return '/about';
+  }
 
-app.get('/about', async (req, res) => {
-  // Here we are retrieving the first document from your API endpoint
-  const document = await client.getSingle('about')
-  res.render('pages/about', { document: document.data })
+  // Default to homepage
+  return '/';
+};
 
-  // console.log(document.data)
-})
+// Middleware to inject prismic context
+app.use((req, res, next) => {
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`)
+  // whats this?
+  const ua = UAParser(req.headers['user-agent']);
+
+  // whats this?
+  res.locals.isDesktop = ua.device.type === undefined;
+  res.locals.isPhone = ua.device.type === 'mobile';
+  res.locals.isTablet = ua.device.type === 'tablet';
+
+  // whats this?
+  res.locals.Link = HandleLinkResolver;
+  res.locals.PrismicH = PrismicH;
+  res.locals.Numbers = (index) => {
+    return index === 0
+      ? 'One'
+      : index === 1
+      ? 'Two'
+      : index === 2
+      ? 'Three'
+      : index === 3
+      ? 'Four'
+      : '';
+  };
+
+  next();
 });
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.locals.basedir = app.get('views');
 
+const handleRequest = async (api) => {
+  // what is results?
+  const [meta, preloader, navigation, home, about, { results: collections }] =
+  await Promise.all([
+    api.getSingle('meta'),
+    api.getSingle('preloader'),
+    api.getSingle('navigation'),
+    api.getSingle('home'),
+    api.getSingle('about'),
+    api.query(Prismic.predicate.at('document.type', 'collection'), {
+      fetchLinks: 'product.image',
+    }),
+  ]);
 
+  //   console.log(about, home, collections);
 
+  const assets = [];
 
+  // what is happening here?
+  home.data.gallery.forEach((item) => {
+    assets.push(item.image.url);
+  });
 
+  about.data.gallery.forEach((item) => {
+    assets.push(item.image.url);
+  });
 
+  about.data.body.forEach((section) => {
+    if (section.slice_type === 'gallery') {
+      section.items.forEach((item) => {
+        assets.push(item.image.url);
+      });
+    }
+  });
 
+  collections.forEach((collection) => {
+    collection.data.list.forEach((item) => {
+      assets.push(item.product.data.image.url);
+    });
+  });
 
+  console.log(assets);
 
+  return {
+    assets,
+    meta,
+    home,
+    collections,
+    about,
+    navigation,
+    preloader,
+  };
+};
 
-// // Link Resolver
-// const HandleLinkResolver = (doc) => {
-//   // Define the url depending on the document type
-//   //   if (doc.type === 'page') {
-//   //     return '/page/' + doc.uid;
-//   //   } else if (doc.type === 'blog_post') {
-//   //     return '/blog/' + doc.uid;
-//   //   }
+app.get('/', async (req, res) => {
+  const api = await initApi(req);
+  const defaults = await handleRequest(api);
 
-//   // Default to homepage
-//   return '/';
-// };
+  // what is defaults?
+  res.render('pages/home', {
+    ...defaults,
+  });
+});
 
-// // Middleware to inject prismic context
-// app.use((req, res, next) => {
-//   res.locals.ctx = {
-//     endpoint: process.env.PRISMIC_ENDPOINT,
-//     linkResolver: HandleLinkResolver,
-//   };
-//   res.locals.PrismicH = PrismicH;
+app.get('/about', async (req, res) => {
+  const api = await initApi(req);
+  const defaults = await handleRequest(api);
 
-//   next();
-// });
+  res.render('pages/about', {
+    ...defaults,
+  });
+});
 
+app.get('/collections', async (req, res) => {
+  const api = await initApi(req);
+  const defaults = await handleRequest(api);
 
-// // console.log(process.env.PRISMIC_ENDPOINT, process.env.PRISMIC_CLIENT_ID)
+  res.render('pages/collections', {
+    ...defaults,
+  });
+});
 
-// app.set('view engine', 'pug')
-// app.set('views', path.join(__dirname, 'views'))
-// app.locals.basedir = app.get('views');
+// app.get('/detail/:uid', async (req, res) => {
+//   const api = await initApi(req);
+//   const defaults = await handleRequest(api);
 
-// const handleRequest = async (api) => {
-//   const [meta, home, about, { results: collections }] = await Promise.all([
-//     api.getSingle('meta'),
-//     api.getSingle('home'),
-//     api.getSingle('about'),
-//     api.query(Prismic.predicate.at('document.type', 'collection'), {
-//       fetchLinks: 'product.image',
-//     }),
-//   ]);
-// }
-
-//   app.get('/', async (req, res) => {
-//     const api = await initApi(req);
-//     const defaults = await handleRequest(api);
-
-//     console.log(defaults)
-  
-//     res.render('pages/home', {
-//       ...defaults,
-//     });
+//   const product = await api.getByUID('product', req.params.uid, {
+//     fetchLinks: 'collection.title',
 //   });
 
-// app.get('/about', async (req, res) => {
-//   res.render('pages/about')
-// })
+//   if (product) {
+//     res.render('pages/detail', {
+//       product,
+//       ...defaults,
+//     });
+//   } else {
+//     res.status(404).send('Page not found');
+//   }
+// });
 
-// app.get('/collections', async (req, res) => {
-//   res.render('pages/collections')
-// })
+app.get('/detail/:uid', async (req, res) => {
+  const api = await initApi(req);
+  const defaults = await handleRequest(api);
 
-// app.get('/detail/:id', async (req, res) => {
-//   res.render('pages/detail')
-// })
+  const product = await api.getByUID('product', req.params.uid, {
+    fetchLinks: 'collection.title',
+  });
 
+  res.render('pages/detail', {
+    ...defaults,
+    product,
+  });
+});
 
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});

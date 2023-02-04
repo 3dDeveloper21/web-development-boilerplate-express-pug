39e4a4e4988fd9f3354e7742ae6f259d6e015592
Harpreet Singh <3ddeveloper21@gmail.com>
2023-02-03 19:57:28 -0500

    prismic v5 to v6

---
+++ config/prismicConfig.js
@@ -0,0 +1,19 @@
+// import fetch from 'node-fetch';
+// import * as prismic from '@prismicio/client';
+// require('dotenv').config();
+
+// const repoName = process.env.PRISMIC_ENDPOINT;
+// const accessToken = process.env.PRISMIC_ACCESS_TOKEN;
+
+// const routes = [
+//   {
+//     type: 'home',
+//     path: '/'
+//   },
+// ]
+
+// export const client = prismic.createClient(repoName, {
+//   fetch, 
+//   accessToken,
+//   routes
+// })
